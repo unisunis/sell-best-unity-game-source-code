96945b1090343ec4817659c31f0fda0070c8a1eb
unity game source code <43838225+gamesourcecode@users.noreply.github.com>
2019-09-19 10:33:30 +0700

    just file
    nothing

---
+++ MainUiButtons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement ;
+
+public class MainUiButtons : MonoBehaviour
+{
+    public GameObject optionsCanvas, mainCanvas, questionCanvas, editorCanvas;
+    public ScrollRect scrollPanel;
+    public PopulateQuestionScroll questionScroll;
+
+    private void Start()
+    {
+        optionsCanvas.SetActive(false);
+    }
+
+    public void PauseButtonPressed(bool pausePressed)
+    {
+        optionsCanvas.SetActive(pausePressed);
+    }
+
+    public void CustomEditorButtonPressed()
+    {
+        questionScroll.LoadQuestions();
+        editorCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+        questionCanvas.SetActive(false);
+        optionsCanvas.SetActive(false);
+    }
+
+    public void NewGamemodeButtonPressed()
+    {
+        optionsCanvas.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnToMainGame()
+    {
+        optionsCanvas.SetActive(false);
+        editorCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
+        questionCanvas.SetActive(true);
+    }
+
+    
+}
